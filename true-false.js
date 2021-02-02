88eacbc5bd597065aa453aa2169b94a999fbadb6
jaki vaiya <mdjak8980@gmail.com>
2021-02-02 13:47:18 +0600

    sdfsf

---
+++ true-false.js
@@ -0,0 +1,17 @@
+// falsy
+//false
+//null
+// 0
+//undefined
+//NaN
+
+//truthy
+//"0",number," ", [],{}
+
+const age = 0;
+if(age || age==0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is not true");
+}
